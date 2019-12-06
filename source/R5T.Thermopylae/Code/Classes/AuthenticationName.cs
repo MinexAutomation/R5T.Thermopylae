b8fcef8db69e0b69f3f2bae412c877cbccf708a6
MinexAutomation <minex.automation@gmail.com>
2019-12-06 12:27:35 -0800

    Added authentication name and named basic authentication types.

---
+++ source/R5T.Thermopylae/Code/Classes/AuthenticationName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.Thermopylae
+{
+    /// <summary>
+    /// The name that identifies an authentication.
+    /// </summary>
+    public class AuthenticationName : TypedString
+    {
+        public AuthenticationName(string value)
+            : base(value)
+        {
+        }
+    }
+}
---
+++ source/R5T.Thermopylae/Code/Classes/NamedBasicAuthentication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Thermopylae
+{
+    public class NamedBasicAuthentication : BasicAuthentication
+    {
+        public AuthenticationName Name { get; set; }
+    }
+}
